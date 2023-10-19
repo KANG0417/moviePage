7852da37e13fe488732fc23679a531e19f95f178
KANG0417 <ipia99@naver.com>
2023-10-19 13:14:06 +0900

    Co-authored-by: dygmm4288 <dygmm4288@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 const options = {
   method: 'GET',
   headers: {
@@ -7,10 +6,10 @@ const options = {
   }
 };
 
-const key = "6058bbe97cc057987d5101e107d2daa2";
-const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko-KR&page=3&sort_by=popularity.desc`
+const url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko-KR&page=3&sort_by=popularity.desc";
+
 const fetchMovie = async() => {
-  fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko-KR&page=3&sort_by=popularity.desc', options)
+  fetch(url, options)
     .then(response => response.json())
     .then(response => console.log(response))
     .catch(err => console.error(err));
