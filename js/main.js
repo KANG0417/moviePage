c675df1203a14b080acb945d4da3dd6ec9cb42c5
KANG0417 <ipia99@naver.com>
2023-10-19 20:29:26 +0900

    Co-authored-by: wonee09 <wonee09@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -1,22 +1,54 @@
+// html 태그 가져오기
+const card = document.querySelector("#card");
+
+// API 가져오기
 const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer 6058bbe97cc057987d5101e107d2daa2'
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MDU4YmJlOTdjYzA1Nzk4N2Q1MTAxZTEwN2QyZGFhMiIsInN1YiI6IjY1MmY3MGVjYzk5NWVlMDBlM2Y2OWMwOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iCU00Jf2HYptUo5tLzX0aVoVrVJDze0Fqx8zQk-Dq50'
   }
 };
 
-const url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko-KR&page=3&sort_by=popularity.desc";
+const imgClick = (id) => {
+  alert(`id: ${id}`);
+}
 
-const fetchMovie = async() => {
+const url = "https://api.themoviedb.org/3/movie/top_rated?language=ko-KR&page=1";
+const getMovie = async() => {
   fetch(url, options)
-    .then(response => response.json())
-    .then(response => console.log(response))
+    .then(response => {
+      return response.json();
+    })
+    .then(data =>  {
+      const movies = data.results;
+      const imgUrl = "https://image.tmdb.org/t/p/w500";
+      movies.forEach(item => {
+        // console.log(item);
+        const {
+          title,
+          overview,
+          vote_average,
+          backdrop_path,
+          id,
+        } = item;
+
+        let img = imgUrl + backdrop_path;
+        let cardHtml = `
+        <div id="movie-card-wrap">
+          <img src=${img} alt="영화 이미지" onclick="imgClick(${id})" class="movie-img"></img>
+          <p class="movie-title">${title}</p>
+          <p class="movie-content">${overview}</p>
+          <p class="rating-icon"><span class="movie-rating">${vote_average}</span></p>
+        </div>
+        `;
+
+        if (overview !== "") {
+          card.insertAdjacentHTML("beforeend", cardHtml);
+        }
+      });
+    })
     .catch(err => console.error(err));
-  
-    // 생성하는
-    
   }
-  
   
   getMovie();
