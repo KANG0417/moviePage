eab77f95588520c7463b39c42c914d95bd982815
KANG0417 <ipia99@naver.com>
2023-10-19 09:47:12 +0900

    first push

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer 6058bbe97cc057987d5101e107d2daa2'
+  }
+};
+
+const key = "6058bbe97cc057987d5101e107d2daa2";
+const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko-KR&page=3&sort_by=popularity.desc`
+const fetchMovie = async() => {
+  fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko-KR&page=3&sort_by=popularity.desc', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+  
+    // 생성하는
+    
+  }
+  
+  
+  getMovie();
